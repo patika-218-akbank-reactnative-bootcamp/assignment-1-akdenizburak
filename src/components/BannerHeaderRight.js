c74bda0d76263b16ef840cb4cbb474d961d399bc
akdenizburak <akdenizburakk7@gmail.com>
2022-08-19 03:18:10 +0300

    Created Component BannerHeaderRight

---
+++ src/components/BannerHeaderRight.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from "react-native"
+import BannerHeaderRightDM from './BannerHeaderRightDM';
+import BannerHeaderRightLike from './BannerHeaderRightLike';
+import BannerHeaderRightShare from './BannerHeaderRightShare';
+
+
+
+const BannerHeaderRight=()=>{
+    return(
+        <View style={{flexDirection:"row", width:"40%", backgroundColor:"white",justifyContent:"space-evenly"}}>
+            <BannerHeaderRightShare/>
+            <BannerHeaderRightLike/>
+            <BannerHeaderRightDM/>
+        </View>
+    )
+}
+
+export default BannerHeaderRight;
